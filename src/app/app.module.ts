12d2be6814ee784275061fd7b110689328c1ca53
Gleb <gsh@veeroute.com>
2021-09-23 22:58:23 +0300

    Real world: initial commit. Create project, add ngrx, add taiga Ui, set basic folder structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,32 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {TuiRootModule, TuiDialogModule, TuiNotificationsModule} from "@taiga-ui/core";
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {TuiActionModule} from "@taiga-ui/kit";
+import {StoreModule} from '@ngrx/store';
+import {AuthModule} from "./auth/auth.module";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent
+	],
+	imports: [
+		BrowserModule,
+		AppRoutingModule,
+		TuiRootModule,
+		BrowserAnimationsModule,
+		TuiDialogModule,
+		TuiNotificationsModule,
+		StoreModule.forRoot({}, {}),
+
+		AuthModule
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
+
